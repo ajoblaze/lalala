f83ed64f351421875064d741575c53b5be0bfcb6
Aldibe <aldi3594@gmail.com>
2015-03-09 16:26:58 +0700

    tinggal nunggu database + fungsi database sharusnya :v

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,19 +1,21 @@
 <?php
+require_once "db_user/Db_User.php";
 class IndexController extends Zend_Controller_Action
 {
 	private $url;
 	private $mySession,$config;
+	private $datauser;
 
 	public function init()
 	{
 		$this->url = $this->getRequest()->getBaseURL();
 		$this->view->baseUrl = $this->url;
 
-		$this->mySession = new Zend_Session_Namespace("Latihan");
-		$this->view->mySession = $this->mySession;
+		//$this->mySession = new Zend_Session_Namespace("Latihan");
+		//$this->view->mySession = $this->mySession;
 
-		$this->config = Zend_Registry::get("config");
-		$this->view->config = $this->config;
+		//$this->config = Zend_Registry::get("config");
+		//$this->view->config = $this->config;
 
 
 		$this->initView();
@@ -28,37 +30,38 @@ class IndexController extends Zend_Controller_Action
 		{
 			switch($param->err)
 			{
-				case 1: $error = "Invalid Username / Password";break;
-				case 2: break;
+				case 1: $error = "Invalid Username / Password"; break;
+				case 2: $error = "Registration Successful"; break;
+				case 3: break;
 				default: break;
 			}
 			$this->view->error = $error;
 		}
 	}
 
-	public function doLoginAction()
+	public function dologinAction()
 	{
 		$this->_helper->viewRenderer->setNoRender();
 
 		$param = (object) $this->getRequest()->getPost();
 
-		if($param->txtuser == "user" && $param->txtpass == "user")
-		{
-			$this->mySession->unlock();
-			$this->mySession->username = $param->txtuser;
-			$this->mySession->lock();
+		$datauser = $this->db_user->getUser();
 
-			$this->_redirect("main");
+		foreach($row as $datauser)
+		{
+			if($param->txtuser == $row[""] && $param->txtpass == $)
+			{
+				$this->mySession->unlock();
+				$this->mySession->username = $param->txtuser;
+				$this->mySession->role = $param->txtuser;
+				$this->mySession->lock();
+				$this->_redirect("main");
+			}
 		}
 		else
 		{
 			$this->_redirect("index?err=1");
 		}
 
-	}
-
-	public function mainAction()
-	{
-		
 	}
 }
