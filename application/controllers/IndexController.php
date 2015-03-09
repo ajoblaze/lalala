7b798880d9a9c1db00b4a657a8bf8dbf8fd566d3
Aldibe <aldi3594@gmail.com>
2015-03-09 13:27:31 +0700

    edit di indexController - mainAction (blom kelar), doLoginAction (blom kelar)

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,10 +3,36 @@ class IndexController extends Zend_Controller_Action
 {
 	public function init()
 	{
+		$this->url = $this->getRequest()->getBaseURL();
+		$this->view->baseUrl = $this->url;
+
+		$this->mySession = new Zend_Session_Namespace("Latihan");
+		$this->view->mySession = $this->mySession;
+
+		$this->config = Zend_Registry::get("config");
+		$this->view->config = $this->config;
+
+
+		$this->initView();
 	}
-	
+
 	public function indexAction()
 	{
+
+	}
+
+	public function doLoginAction()
+	{
+		$this->_helper->viewRenderer->setNoRender();
+
+
+
+
+
+	}
+
+	public function mainAction()
+	{
+		
 	}
 }
-
